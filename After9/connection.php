241c542748096e3c33800442ef906715e992f0c5
shashwat <shashwatrane1920@gmail.com>
2023-06-17 22:02:18 +0530

    first commit

---
+++ After9/connection.php
@@ -0,0 +1,4 @@
+<?php
+//$con=mysqli_connect("localhost","id2538044_sajalagrawal","******","id2538044_store") or die(mysqli_error($con));
+$con=mysqli_connect("localhost","root","123123","after9") or die(mysqli_error($con));
+?>
